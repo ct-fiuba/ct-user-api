b40bc55e1c7221006138fc0b115b8d3236bd7fef
Oli <olifer97@gmail.com>
2020-08-04 20:26:57 -0300

    fix error.

--- src/controllers/establishmentsController.js
+++ src/controllers/establishmentsController.js
@@ -1,28 +1,38 @@
-const request = require('request');
+const pipelineResponse = (stream, res) => {
+  stream.on('error', () => {
+    res.statusCode = 500;
+    res.end('Something went wrong');
+  });
+
+  stream.pipe(res)
+}
 
 module.exports = function establishmentsController(visitManagerGateway) {
   const get = async (req, res, next) => {
     let filters = req.query;
     let visitManagerResponse = await visitManagerGateway.findEstablishments(filters)
-    //res.status(visitManagerResponse.status)
-    visitManagerResponse.pipe(res)
+    pipelineResponse(visitManagerResponse, res)
   };
 
   const getSingleEstablishment = async (req, res, next) => {
-    return visitManagerGateway.findEstablishment(req.params.establishmentId).then(response => res.send(response))
+    let visitManagerResponse = await visitManagerGateway.findEstablishment(req.params.establishmentId)
+    pipelineResponse(visitManagerResponse, res)
   };
 
   const add = async (req, res, next) => {
-    return visitManagerGateway.addEstablishment(req.body).then(response => res.send(response))
+    let visitManagerResponse = await visitManagerGateway.addEstablishment(req.body)
+    pipelineResponse(visitManagerResponse, res)
   };
 
   const update = async (req, res, next) => {
     establishmentId = req.params.establishmentId;
-    return visitManagerGateway.updateEstablishment(establishmentId, req.body).then(response => res.send(response))
+    let visitManagerResponse = await visitManagerGateway.updateEstablishment(establishmentId, req.body)
+    pipelineResponse(visitManagerResponse, res)
   };
 
   const remove = async (req, res, next) => {
-    return visitManagerGateway.deleteEstablishment(req.params.establishmentId).then(response => res.send(response))
+    let visitManagerResponse = await visitManagerGateway.deleteEstablishment(req.params.establishmentId)
+    pipelineResponse(visitManagerResponse, res)
   };
 
   return {
