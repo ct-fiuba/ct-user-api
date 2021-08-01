d4219cd1f7f936e312ac9dc55e7d89db805bafd6
Oli <olifer97@gmail.com>
2021-08-01 15:42:32 -0300

    typo

--- src/gateways/codesWhispererGateway.js
+++ src/gateways/codesWhispererGateway.js
@@ -2,12 +2,12 @@ const got = require('got');
 
 module.exports = function visitManagerGateway() {
 
-  const visitManagerAPI = got.extend({
+  const codesWhispererAPI = got.extend({
     prefixUrl: process.env.CODES_WHISPERER_URL
   });
 
   const getBillboard = async filters => {
-    return visitManagerAPI.stream.get('billboard', { searchParams: filters })
+    return codesWhispererAPI.stream.get('billboard', { searchParams: filters })
   };
 
   return {
